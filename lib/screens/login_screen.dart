f08b2fb7d8eaffaf53b024cd02f433bb5ab5220c
Pradana098 <pradanadimas534@gmai.com>
2025-04-05 21:48:04 +0700

    tes commit ulang

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -124,11 +124,6 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const Text(
-            "Login Anggota",
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
           const SizedBox(height: 20),
           _inputField(Icons.email, "Email", emailController, false),
           const SizedBox(height: 16),
